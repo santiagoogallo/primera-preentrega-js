2b61b81d217f1e062a56b1a499d4942e7ac12714
santiago gallo <santiago.matias.gallo@gmail.com>
2023-09-27 00:04:12 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,27 @@
+
+
+/*let usario = parseInt(prompt("Ingrese un uruario: "))
+
+function solicitarNombre (){
+    for(let = i; i <= 3; i==){
+    let resultado = usuario * i; 
+    alert("El usuario ingresado es " + resultado + "Bienvenido " + usuario "!")
+    }break
+}
+
+solicitarNombre();*/
+
+
+
+
+
+
+let precioProducto = 3000
+let precioIngresado = parseFloat(prompt("Ingrese un monto"))
+
+while (precioProducto == precioIngresado){
+    alert("Muchas gracias por tu compra! $" + precioIngresado)
+    break
+    }
+
+/*let usuario = parseInt(prompt("Ingrese un usuario: "))*/
