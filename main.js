5abc7f32b44572822a840daec717dcf6a1c21062
santiago gallo <santiago.matias.gallo@gmail.com>
2023-09-27 13:42:08 -0300

    Usuario agregado

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
 
-let nombreIngresado   = prompt("Ingresar nombre");
-let apellidoIngresado = prompt("Ingresar apellido");
+let nombreUsuario   = prompt("Ingresar nombre:");
+let apellidoUsuario = prompt("Ingresar apellido:");
 
-if((nombreIngresado !="") && (apellidoIngresado !="")){
-    alert("Nombre: "+nombreIngresado +"\nApellido: "+apellidoIngresado); 
+if((nombreUsuario !="") && (apellidoUsuario !="")){
+    alert("Bienvenido " + nombreUsuario + " " + apellidoUsuario + "!"); 
 }else{
     alert("Error: Ingresar nombre y apellido");
 }
@@ -14,10 +14,17 @@ if((nombreIngresado !="") && (apellidoIngresado !="")){
 
 
 let precioProducto = 3000
-let precioIngresado = parseFloat(prompt("Ingrese un monto"))
+let precioIngresado = parseFloat(prompt("Nustros productos estan $3000, ingrese un monto para continuar"))
 
 while (precioProducto == precioIngresado){
-    alert("Muchas gracias por tu compra! $" + precioIngresado)
+    alert("Muchas gracias por tu compra " + nombreUsuario + "!")
     break
-    }
+    }if (precioIngresado < precioProducto) {
+        alert("El monto es insuficiente, vuelve a ingresar un precio")
+        
+    } 
 
+/*if (precioIngresado < precioProducto){
+alert("El monto es insuficiente, vuelve a ingresar un precio")
+}if ( precioIngresado > precioProducto )
+*/
