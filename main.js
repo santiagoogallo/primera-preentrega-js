78fc031ae2a9ad18da8814ea1636467d93b51e72
santiago gallo <santiago.matias.gallo@gmail.com>
2023-09-29 17:37:19 -0300

    Correcciones finales

--- main.js
+++ main.js
@@ -1,30 +1,33 @@
 
 
-let nombreUsuario   = prompt("Ingresar nombre:");
+
+
+let nombreUsuario = prompt("Ingresar nombre:");
 let apellidoUsuario = prompt("Ingresar apellido:");
 
-if((nombreUsuario !="") && (apellidoUsuario !="")){
+if (nombreUsuario !== "" && apellidoUsuario !== "") {
     alert("Bienvenido " + nombreUsuario + " " + apellidoUsuario + "!"); 
-}else{
-    alert("Error: Ingresar nombre y apellido");
+
+    let precioIngresado = parseFloat(prompt("Nuestros productos están $3000, ingrese un monto para continuar"));
+
+    function realizarCompra(nombre, precioProducto) {
+        while (true) {
+            if (isNaN(precioIngresado)) {
+                alert("Por favor, ingrese un monto válido.");
+            } else if (precioIngresado >= precioProducto) {
+                alert("Muchas gracias por tu compra, " + nombre + "!");
+                break;
+            } else {
+                alert("El monto es insuficiente, vuelve a ingresar un precio.");
+            }
+        
+            precioIngresado = parseFloat(prompt("Nuestros productos están $3000, ingrese un monto para continuar"));
+        }
+    }
+
+    realizarCompra(nombreUsuario, 3000);
 }
 
 
 
 
-
-let precioProducto = 3000
-let precioIngresado = parseFloat(prompt("Nustros productos estan $3000, ingrese un monto para continuar"))
-
-while (precioProducto == precioIngresado){
-    alert("Muchas gracias por tu compra " + nombreUsuario + "!")
-    break
-    }if (precioIngresado < precioProducto) {
-        alert("El monto es insuficiente, vuelve a ingresar un precio")
-        
-    } 
-
-/*if (precioIngresado < precioProducto){
-alert("El monto es insuficiente, vuelve a ingresar un precio")
-}if ( precioIngresado > precioProducto )
-*/
